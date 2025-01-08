5593c4c60caaf18f778271c36bfaf4cd0a3b1c98
Irina Lebedeva <hataketodoroke@gmail.com>
2025-01-08 23:47:06 +0300

    hw-1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Message from './message/Message'
 import MessageSender from './message-sender/MessageSender'
 import s2 from '../../s1-main/App.module.css'
@@ -14,31 +14,45 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: UserType
+    message: TextMessageType
+}
+
+type UserType = {
+    avatar: string
+    name: string
+}
+
+type TextMessageType = {
+    text: string
+    time: string
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
-    id: 0,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
-    },
-    message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
-    },
-}
+  id: 0,
+  user: {
+    avatar: avatar, // можно менять
+    name: "Ivan", // можно менять
+  },
+  message: {
+    text: "Hello, she didn’t do anything and rested all day, how are you?", // можно менять
+    time: "09:01", // можно менять
+  },
+};
 export const friendMessage0: MessageType = {
-    id: 100,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
-    },
-    message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
-    },
-}
+  id: 100,
+  user: {
+    avatar: avatar, // можно менять
+    name: "Ivan", // можно менять
+  },
+  message: {
+    text: "Hello, how are you, what did you do yesterday?", // можно менять
+    time: "09:00", // можно менять
+  },
+};
 
 const HW1 = () => {
     return (
